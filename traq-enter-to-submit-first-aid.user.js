fa9006f55e1870fdb8fc0c24ac019b3990c828a3
tqkoh <shktmygs@gmail.com>
2022-09-18 08:36:56 +0900

    update

--- traq-enter-to-submit-first-aid.user.js
+++ traq-enter-to-submit-first-aid.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         traQ enter to submit first aid
 // @namespace    https://github.com/tqkoh/traQ-enter-to-submit-first-aid
-// @version      0.0.2
+// @version      0.0.3
 // @description  edge, chrome v105+ で traQ の enter でメッセージを送信できないバグの応急処置
 // @author       tqk
 // @match        https://q.trap.jp/channels/*
@@ -12,7 +12,7 @@
 (function() {
 	'use strict';
 	document.addEventListener('keydown', function (event) {
-		if (event.key=='Enter'){
+		if (event.key=='Enter' && !event.ctrlKey && !event.altKey && !event.shiftKey){
 			document.querySelectorAll('[data-testid="message-send-button"]')[0].click();
 		}
 	}, false);
